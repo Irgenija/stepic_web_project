0fd68aa339a03cba7cdf7f4218997dc5bf399a77
Irgenija <irgenija89@gmail.com>
2020-07-18 15:51:02 +0300

    remove redundant encoding

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ def app(environ, start_response):
     headers = [('Content-Type', 'text/plain')]
     body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
     start_response(status, headers)
-    return [bytes(body, 'utf-8')]
+    return [bytes(body)]
