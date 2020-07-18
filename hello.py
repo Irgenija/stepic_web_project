97db0098cb6f0dd109aa607d5af6f286f6da8280
Irgenija <irgenija89@gmail.com>
2020-07-18 15:20:29 +0300

    config fixups

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-def app(environ, start_responce):
+def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
-    start_responce(status, headers)
+    start_response(status, headers)
     return body
